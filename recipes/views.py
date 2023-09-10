af89539225b55eda2ac4a4cdc3b3a1fd550f7fd1
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-09-10 10:57:36 -0300

    Using "faker" to simulate different texts for card

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.random_factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(15)],
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
