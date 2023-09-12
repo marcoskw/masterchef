62c4d43cd39367cb8311d577d9b6a3f9dd3a67d1
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-09-12 18:57:42 -0300

    Initial adjustment to show views in templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from utils.recipes.random_factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(15)],
+        'recipes': recipes,
+        # 'recipes': [make_recipe() for _ in range(15)],         # example with randon content
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 
