29ee72b86750623c42fdc6b8803b5a146a46c73f
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-09-17 21:30:39 -0300

    Optimizing 404 error in categories

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.random_factory import make_recipe
 from .models import Recipe
 from django.http import Http404
@@ -16,17 +16,15 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
-
-    if not recipes:
-        raise Http404('Página não encontrada')
-
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name}'
+        'title': f'{recipes[0].category.name}'
     })
 
 
