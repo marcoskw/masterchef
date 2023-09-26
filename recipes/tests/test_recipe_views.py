591a72b461b43a3b013852f0ccf2ec0c8d013ccb
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-09-26 17:18:32 -0300

    Create test_recipe_base

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,11 +1,11 @@
 from recipes import views
-from django.test import TestCase
 from django.urls import reverse, resolve
-from recipes.models import Category, Recipe
-from django.contrib.auth.models import User
+
+from recipes.models import Recipe
+from .test_recipe_base import RecipeTestBase
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -19,39 +19,21 @@ class RecipeViewsTest(TestCase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_home_template_shows_no_recipes_founds_if_no_recipes(self):
+
         response = self.client.get(reverse('recipes:home'))
         self.assertIn(
             '<h1>Sem receitas para mostrar :-(</h1>',
             response.content.decode('utf-8'))
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(
-            name='Category'
-        )
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='user@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Test Title',
-            description='Recipe Test Description',
-            slug='recipe-test-slug',
-            preparation_time='10',
-            preparation_time_unit='test',
-            servings='5',
-            servings_unit='test',
-            preparation_ingredients='1 Test, 2 Test',
-            preparation_steps='1 Test, 2 Test, 3 Test',
-            is_published=True,
-        )
+        # Need a recipefor this test
+        self.make_recipe()
+
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
+
+        # Check if one recipe exists
         self.assertIn('Recipe Test Title', content)
         self.assertIn('10 test', content)
         self.assertIn('5 test', content)
