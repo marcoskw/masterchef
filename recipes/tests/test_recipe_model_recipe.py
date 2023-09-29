f65bdc24a2d2fdd39ae49e41eb3f62194a912673
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-09-29 08:39:27 -0300

    Default test is_published

---
+++ recipes/tests/test_recipe_model_recipe.py
@@ -0,0 +1,45 @@
+from .test_recipe_base import RecipeTestBase, Recipe
+from django.core.exceptions import ValidationError
+from parameterized import parameterized
+
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test Categoria Default'),
+            author=self.make_author(username='newuser'),
+            title='Recipe Test Title',
+            description='Recipe Test Description',
+            slug='recipe-test-slug',
+            preparation_time='10',
+            preparation_time_unit='test',
+            servings='5',
+            servings_unit='test',
+            preparation_ingredients='1 Test, 2 Test',
+            preparation_steps='1 Test, 2 Test, 3 Test',
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
+
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_lenght(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()  # validation
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.is_published,
+            msg='Recipe is_published is not False',
+        )
