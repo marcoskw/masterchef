4344d66301f966dbe8bfb6f2847a491325bfdead
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-10-02 19:34:43 -0300

    Finalizing testing and research implementation

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.random_factory import make_recipe
+from django.db.models import Q
 from .models import Recipe
 from django.http import Http404
 
@@ -45,7 +46,17 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+
+        ),
+        is_published=True
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
