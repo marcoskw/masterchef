a996bc8a4edd8b9eda329555c7b4e0fd34ad2a15
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-10-10 21:18:29 -0300

    Applying adjustments to password encryption

--- authors/views.py
+++ authors/views.py
@@ -20,7 +20,9 @@ def register_create(request):
     form = RegisterForm(request.POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Usuário criado com sucesso')
 
         del (request.session['register_form_data'])
