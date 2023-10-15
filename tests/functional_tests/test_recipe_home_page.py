58c5e399e88efdf89b671ad9c4bc9f5ab0654a77
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-10-15 08:06:10 -0300

    Change initial selenium test

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -4,14 +4,21 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.by import By
 
 
-class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
     def sleep(self, seconds=5):
         time.sleep(seconds)
 
-    def test_recipe_home_page_with_no_recipes(self):
-        browser = make_chrome_browser()
-        browser.get(self.live_server_url)
-        self.sleep()
-        body = browser.find_element(By.TAG_NAME, 'body')
+
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    def test_recipe_home_page_without_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('Sem receitas para mostrar :-(', body.text)
-        browser.quit
