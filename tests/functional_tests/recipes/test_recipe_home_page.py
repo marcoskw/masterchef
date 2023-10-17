136d2ab73563bebe589bcb6ce732e41180affb5c
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-10-17 20:52:05 -0300

    Test to find specific recipe

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,6 +1,8 @@
 import pytest
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from .base import RecipeBaseFunctionalTest
+from unittest.mock import patch
 
 
 @pytest.mark.functional_test
@@ -9,3 +11,22 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('Sem receitas para mostrar :-(', body.text)
+
+    def test_recipe_search_input_can_find_correct_recipe(self):
+        title_needed = 'Title of Recipe'
+
+        recipes = self.make_recipe_in_bath()
+
+        recipes[0].title = 'Title of Recipe'
+        recipes[0].save()
+
+        self.browser.get(self.live_server_url)
+
+        search_input = self.browser.find_element(
+            By.XPATH, '//input[@placeholder="Procurar por uma receita..."]')
+
+        search_input.send_keys(recipes[0].title)
+        search_input.send_keys(Keys.ENTER)
+
+        self.assertIn(title_needed, self.browser.find_element(
+            By.CLASS_NAME, 'main-content-list').text)
