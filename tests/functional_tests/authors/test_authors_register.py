86588e31dac740894d3e8c3709a1b560a32b5a1c
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-10-18 22:30:57 -0300

    Create test first name empty message

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -1,6 +1,36 @@
 from .base import AuthorsBaseTest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class AuthorsRegisterTest(AuthorsBaseTest):
-    def test_open_authors_register(self):
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(
+            By.XPATH, f'//input[@placeholder="{placeholder}"]'
+        )
+
+    def fill_form_dummy_data(self, form):
+        fields = form.find_elements(By.TAG_NAME, 'input')
+
+        for field in fields:
+            if field.is_displayed():
+                field.send_keys(' ' * 20)
+
+    def test_empty_first_name_error_message(self):
         self.browser.get(self.live_server_url + '/authors/register')
+
+        form = self.browser.find_element(
+            By.XPATH, '/html/body/main/form/div[1]'
+        )
+        self.fill_form_dummy_data(form)
+        form.find_element(By.NAME, 'email').send_keys('teste@teste.com')
+
+        first_name_field = self.get_by_placeholder(form, 'Nome')
+        first_name_field.send_keys(' ')
+        first_name_field.send_keys(Keys.ENTER)
+
+        form = self.browser.find_element(
+            By.XPATH, '/html/body/main/form/div[1]'
+        )
+        self.sleep(15)
+        self.assertIn('Escreva seu nome', form.text)
