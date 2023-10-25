e5079df99e52dd1b4fe9a8da40955a60781ca196
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-10-25 18:03:40 -0300

    Creare RecipeAdminArea

--- recipes/admin.py
+++ recipes/admin.py
@@ -9,7 +9,42 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = [
+        'id',
+        'title',
+        'category',
+        'created_at',
+        'is_published',
+    ]
+
+    list_display_links = [
+        'title',
+        'created_at',
+    ]
+
+    search_fields = [
+        'id',
+        'title',
+        'category',
+        'description',
+        'slug',
+    ]
+
+    list_filter = [
+        'category',
+        'author',
+        'is_published',
+    ]
+
+    list_per_page = 30
+
+    list_editable = [
+        'is_published'
+    ]
+
+    ordering = [
+        '-id'
+    ]
 
 
 admin.site.register(Category, CategoryAdmin)
