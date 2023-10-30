2cca378e7749d702e598ce4dc2f386267e33e3dd
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-10-30 13:07:26 -0300

    Adding CSS in Recipe Author

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -27,3 +27,23 @@ class AuthorRecipeForm(forms.ModelForm):
             'cover',
             'category',
         ]
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Porções', 'Porções'),
+                    ('Unidades', 'Unidades'),
+                    ('Pessoas', 'Pessoas'),
+                )
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutos', 'Minutos'),
+                    ('Horas', 'Horas'),
+                )
+            ),
+        }
