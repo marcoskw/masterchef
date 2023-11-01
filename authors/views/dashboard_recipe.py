b5d27bc730a59599aef7d3a66c39761eec57c7da
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-11-01 17:44:59 -0300

    Adding login_required in Class Based Views Recipe

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+from django import http
 from authors.forms.recipe_form import AuthorRecipeForm
 from django.contrib import messages
 from django.http.response import Http404
@@ -5,9 +8,24 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
 from recipes.models import Recipe
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
+@method_decorator(
+    login_required(login_url='authors:login', redirect_field_name='next'),
+    name='dispatch',
+)
 class DashboardRecipe(View):
+    def __init__(self, **kwargs: Any):
+        super().__init__(**kwargs)
+
+    def setup(self, *args, **kwargs):
+        return super().setup(*args, **kwargs)
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
     def get_recipe(self, id=None):
         recipe = None
 
