49295b36c66bafc25709f3cd76adad4579c7570d
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-12-07 22:04:14 -0300

    Separate settings.py

---
+++ masterchef/settings/__init__.py
@@ -0,0 +1,13 @@
+# flake8:noqa
+
+from .enviroment import *
+from .installed_apps import *
+from .middlewares import *
+from .templates import *
+from .databases import *
+from .security import *
+from .i18n import *
+from .assets import *
+
+if True:
+    from .debug_toolbar import *
